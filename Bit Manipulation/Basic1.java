9fccc7394e6ccb2e7fee575adc96b8b8931451e1
Yash verma <yashverma9084+github@gmail.com>
2025-09-01 19:22:14 +0530

    commit

--- Bit Manipulation/Basic1.java
+++ Bit Manipulation/Basic1.java
@@ -3,5 +3,8 @@
 public class Basic1 {
    public static void main(String[] args) {
     System.out.println(67&78);
+    System.out.println(67|78);
+    System.out.println(67^78);
+    System.out.println(~67);
    } 
 }
