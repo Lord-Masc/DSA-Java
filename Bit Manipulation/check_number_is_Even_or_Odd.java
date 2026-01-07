ee37aa3d15206627eda73ec8009eea6485abbad8
Yash verma <yashverma9084+github@gmail.com>
2026-01-07 23:38:51 +0530

    check the number is even or odd

---
+++ Bit Manipulation/check_number_is_Even_or_Odd.java
@@ -0,0 +1,7 @@
+public class check_number_is_Even_or_Odd {
+    public static void main(String[] args) {
+        int num =44;
+        if((num&1)==1) System.out.println("Number is odd");
+        else System.err.println("Number is even");
+    }
+}
